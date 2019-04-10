831ea680830f5a92bcd1ae4bff65b1eeab0d798d
Lucas Rüesch <lucas.rueesch@gmail.com>
2019-04-10 19:06:21 +0200

    Grundstruktur für FX implementierung via SceneBuilder

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,52 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
+		
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.primaryStage = primaryStage;
+		mainWindow();
+			
 	}
 	
+	public void mainWindow() {
+		try {
+			
+			//FXML Datei kann direkt im SceneBuilder geöffnet werden
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+			AnchorPane pane = loader.load();
+			
+			primaryStage.setMinHeight(720.00);
+			primaryStage.setMinWidth(1080.00);
+			
+			//Für jede FXML Datei braucht es einen Controller
+			MainWindowController mainWindowController = loader.getController();
+			mainWindowController.setMain(this);
+						
+			Scene scene = new Scene(pane);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+	}
+	 
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+	
---
+++ src/application/MainWindowController.java
@@ -0,0 +1,14 @@
+package application;
+
+public class MainWindowController {
+
+	// Instanz der Main Klasse
+	public Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+		
+	}
+	
+		
+}
