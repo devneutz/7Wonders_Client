969125af3b9fc7c547513cd38f6da9e4ad38671f
Lucas Rüesch <lucas.rueesch@gmail.com>
2019-04-21 11:17:29 +0200

    Controller anbindungen

--- src/ControllerFXML/MainViewController.java
+++ src/ControllerFXML/MainViewController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -35,6 +36,8 @@ public class MainViewController implements Initializable{
 		       Stage stage = new Stage();
 		       stage.setScene(new Scene(root1));  
 		       stage.show();
+		       
+		       ((Node)event.getSource()).getScene().getWindow().hide();
 		        
 		   } catch(Exception e) {
 		       e.printStackTrace();
@@ -42,8 +45,19 @@ public class MainViewController implements Initializable{
 	}
 	
 	@FXML
-	public void handlePlayAsGuestButton(){
-		
+	public void handlePlayAsGuestButton (ActionEvent event){
+		try {
+		       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ViewFXML/LobbyView.fxml"));
+		       Parent root1 = (Parent) fxmlLoader.load();
+		       Stage stage = new Stage();
+		       stage.setScene(new Scene(root1));  
+		       stage.show();
+		       
+		       ((Node)event.getSource()).getScene().getWindow().hide();
+		        
+		   } catch(Exception e) {
+		       e.printStackTrace();
+		   }
 	}
 
 	@Override
