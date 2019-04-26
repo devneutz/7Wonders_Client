fa9ce33ff52deb37b0b84e4b916446c94c4c0c03
Lucas Rüesch <lucas.rueesch@gmail.com>
2019-04-26 08:49:37 +0200

    alle View und Controller klassen für Lobby erstellt und verknüpft

---
+++ src/ControllerFXML/AdminInLobbyViewController.java
@@ -0,0 +1,39 @@
+package ControllerFXML;
+
+import application.ClientApplicationMain;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminInLobbyViewController {
+	
+public ClientApplicationMain main;
+	
+	public void setMain(ClientApplicationMain main) {
+		this.main = main;
+	}
+	
+	public void handleDeleteLobbyButton(ActionEvent event) {
+		try {
+		       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ViewFXML/LobbyView.fxml"));
+		       Parent root1 = (Parent) fxmlLoader.load();
+		       Stage stage = new Stage();
+		       stage.setScene(new Scene(root1));  
+		       stage.show();
+		       
+		       ((Node)event.getSource()).getScene().getWindow().hide();
+		        
+		   } catch(Exception e) {
+		       e.printStackTrace();
+		   }
+		
+	}
+	
+	public void handleStartLobbyButton() {
+		
+	}
+	
+}
--- src/ControllerFXML/CreateLobbyController.java
+++ src/ControllerFXML/CreateLobbyController.java
@@ -1,6 +1,12 @@
 package ControllerFXML;
 
 import application.ClientApplicationMain;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class CreateLobbyController {
 	
@@ -12,11 +18,35 @@ public class CreateLobbyController {
 	}
 	
 	
-	public void handleCreateLobbyCancelButton() {
+	public void handleCreateLobbyCancelButton(ActionEvent event) {
+		try {
+		       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ViewFXML/LobbyView.fxml"));
+		       Parent root1 = (Parent) fxmlLoader.load();
+		       Stage stage = new Stage();
+		       stage.setScene(new Scene(root1));  
+		       stage.show();
+		       
+		       ((Node)event.getSource()).getScene().getWindow().hide();
+		        
+		   } catch(Exception e) {
+		       e.printStackTrace();
+		   }
 		
 	}
 
-	public void handleCreateLobbyOkButton() {
+	public void handleCreateLobbyOkButton(ActionEvent event) {
+		try {
+		       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ViewFXML/AdminInLobbyView.fxml"));
+		       Parent root1 = (Parent) fxmlLoader.load();
+		       Stage stage = new Stage();
+		       stage.setScene(new Scene(root1));  
+		       stage.show();
+		       
+		       ((Node)event.getSource()).getScene().getWindow().hide();
+		        
+		   } catch(Exception e) {
+		       e.printStackTrace();
+		   }
 		
 	}
 	
