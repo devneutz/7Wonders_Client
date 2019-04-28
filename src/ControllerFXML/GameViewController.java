9adfff51939b2cc26abc16476254423ace4b84a4
Lucas Rüesch <lucas.rueesch@gmail.com>
2019-04-28 08:28:33 +0200

    GameView, GameController & Karten Import

---
+++ src/ControllerFXML/GameViewController.java
@@ -0,0 +1,66 @@
+package ControllerFXML;
+
+import application.ClientApplicationMain;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameViewController {
+	
+	public ClientApplicationMain main;
+
+	public Button UmmunzenButton;
+	public Button RessourceVerwendenButton;
+	public Button ZumBauVerwendenButton;
+	public Label Player1Label;
+	public Label Player2Label;
+	public Label Player3Label;
+	public Label Player4Label;
+	public Label Player5Label;
+	public Label Player6Label;
+	
+
+	
+	public void setMain(ClientApplicationMain main) {
+		this.main = main;
+	}
+	
+	public void handleUmmunzenButton() {
+		
+	}
+	
+	public void handleRessourceVerwendenButton() {
+		
+	}
+	
+	public void handleZumBauVerwendenButton() {
+		
+	}
+	
+	public void handlePlayer1Label() {
+		
+	}
+	
+	public void handlePlayer2Label() {
+		
+	}
+	
+	public void handlePlayer3Label() {
+		
+	}
+	
+	public void handlePlayer4Label() {
+		
+	}
+	
+	public void handlePlayer5Label() {
+		
+	}
+	
+	public void handlePlayer6Label() {
+		
+	}
+	
+		
+	
+
+}
