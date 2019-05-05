5ed9ca2e8778163b43ca1a689f07312a76fd6589
Joel N <joel.neutzner@hotmail.com>
2019-05-05 14:01:08 +0200

    Bugfix Lobby erstellen

--- src/application/ClientModel.java
+++ src/application/ClientModel.java
@@ -18,11 +18,11 @@ import javafx.collections.ObservableList;
 public class ClientModel {
 	private Socket socket;
 	private IPlayer player;
-	
+
 	private ObjectProperty<ObservableList<IPlayer>> LobbyPlayers;
 	private ObjectProperty<ObservableList<ILobby>> Lobbies;
 	private ObjectProperty<Message> lastReceivedMessage;
-	
+
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
 
@@ -34,23 +34,23 @@ public class ClientModel {
 	public ObjectProperty<ObservableList<IPlayer>> getOpponentsListProperty() {
 		return this.LobbyPlayers;
 	}
-	
+
 	public void setPlayer(IPlayer inPlayer) {
 		this.player = inPlayer;
 	}
-	
+
 	public IPlayer getPlayer() {
 		return this.player;
 	}
-	
-	public ObjectProperty<ObservableList<ILobby>> getLobbyListProperty(){
+
+	public ObjectProperty<ObservableList<ILobby>> getLobbyListProperty() {
 		return this.Lobbies;
 	}
-	
+
 	public ObjectProperty<Message> getLastReceivedMessage() {
 		return this.lastReceivedMessage;
 	}
-	
+
 	/**
 	 * Verbindung zum Server aufbauen
 	 * 
@@ -58,89 +58,106 @@ public class ClientModel {
 	 * @param inPort
 	 */
 	public void connect(String inIpAddress, int inPort) {
-		try {
+		try
+		{
 			LobbyPlayers = new SimpleObjectProperty<ObservableList<IPlayer>>();
 			LobbyPlayers.setValue(FXCollections.observableArrayList());
 			Lobbies = new SimpleObjectProperty<ObservableList<ILobby>>();
 			Lobbies.setValue(FXCollections.observableArrayList());
 			this.lastReceivedMessage = new SimpleObjectProperty<Message>();
 			socket = new Socket(inIpAddress, inPort);
-			out = new ObjectOutputStream(socket.getOutputStream()); 
+			out = new ObjectOutputStream(socket.getOutputStream());
 			in = new ObjectInputStream(socket.getInputStream());
 			this.startMessageObserving();
-		} catch (Exception inEx) {
+		} catch (Exception inEx)
+		{
 
 		}
 	}
-	
+
 	public void sendMessage(Message inMessage) {
-		try {
+		try
+		{
 			out.writeObject(inMessage);
 			out.flush();
-		}
-		catch(Exception inEx) {
+		} catch (Exception inEx)
+		{
 			inEx.printStackTrace();
 		}
 	}
-	
+
 	private void handlePlayerJoinedMessage(ServerLobbyMessage inMessage) {
-		Platform.runLater(new Runnable() {						
-			public void run() {	
+		Platform.runLater(new Runnable() {
+			public void run() {
 				LobbyPlayers.getValue().add(inMessage.getPlayer());
 			}
 		});
 	}
-	
+
 	private void handlePlayerLeftMessage(ServerLobbyMessage inMessage) {
-		Platform.runLater(new Runnable() {						
-			public void run() {	
+		Platform.runLater(new Runnable() {
+			public void run() {
 				LobbyPlayers.getValue().remove(inMessage.getPlayer());
 			}
-		});	
+		});
 	}
-	
+
 	private void handleLobbyCreatedMessage(ServerLobbyMessage inMessage) {
-		Platform.runLater(new Runnable() {						
-			public void run() {	
+		Platform.runLater(new Runnable() {
+			public void run() {
 				Lobbies.getValue().add(inMessage.getLobby());
 			}
-		});		
+		});
 	}
-	
+
 	private void handleLobbyDeletedMessage(ServerLobbyMessage inMessage) {
-		Platform.runLater(new Runnable() {						
-			public void run() {	
+		Platform.runLater(new Runnable() {
+			public void run() {
 				Lobbies.getValue().remove(inMessage.getLobby());
 			}
 		});
-		
-		// Ist die gelöschte Lobby diejenige, in der ich mich aktuell befinde? In diesem Fall muss die View über die 'lastReceivedMessage' informiert werden
-		if(inMessage.getLobby().getLobbyName() == this.player.getLobby().getLobbyName()) {
+
+		// Ist die gelöschte Lobby diejenige, in der ich mich aktuell befinde? In diesem
+		// Fall muss die View über die 'lastReceivedMessage' informiert werden
+		if (inMessage.getLobby().getLobbyName() == this.player.getLobby().getLobbyName())
+		{
 			lastReceivedMessage.setValue(inMessage);
 		}
 	}
-	
+
 	/**
-	 * Beim Startup handelt es sich um Login und Registrierungsprozesse. Diese werden in den Controllern durch entsprechende Listener 
-	 * abgehandelt, da darauf zumeist eine UI Interaktion stattfindet.
+	 * Beim Startup handelt es sich um Login und Registrierungsprozesse. Diese
+	 * werden in den Controllern durch entsprechende Listener abgehandelt, da darauf
+	 * zumeist eine UI Interaktion stattfindet.
+	 * 
 	 * @param inMessage
 	 */
 	private void handleStartupMessages(ServerStartupMessage inMessage) {
-		// Die letzte erhaltene Message wird gesetzt sprich, das Handling wird über den Listener gesteuert.
+		// Die letzte erhaltene Message wird gesetzt sprich, das Handling wird über den
+		// Listener gesteuert.
 		lastReceivedMessage.setValue(inMessage);
 	}
 	
+	private void handleDefaultMessges(Message inMessage) {
+		lastReceivedMessage.setValue(inMessage);
+	}
+
 	private void startMessageObserving() {
-		try {
+		try
+		{
 			Runnable r = new Runnable() {
 				@Override
 				public void run() {
-					// 
-					while (true) {
-						try {
-							Message tmpMessage = (Message)in.readObject();
-							if(tmpMessage instanceof ServerLobbyMessage) {
-								switch(((ServerLobbyMessage) tmpMessage).getAction()) {
+					//
+					while (true)
+					{
+						try
+						{
+							Message tmpMessage = (Message) in.readObject();
+							if (tmpMessage instanceof ServerLobbyMessage)
+							{
+								switch (((ServerLobbyMessage) tmpMessage).getAction())
+								{
 								case PlayerJoined:
 									handlePlayerJoinedMessage((ServerLobbyMessage) tmpMessage);
 									break;
@@ -148,21 +165,24 @@ public class ClientModel {
 									handlePlayerLeftMessage((ServerLobbyMessage) tmpMessage);
 									break;
 								case LobbyCreated:
-									handleLobbyCreatedMessage((ServerLobbyMessage) tmpMessage);					
+									handleLobbyCreatedMessage((ServerLobbyMessage) tmpMessage);
 									break;
 								case LobbyDeleted:
 									handleLobbyDeletedMessage((ServerLobbyMessage) tmpMessage);
 									break;
 								default:
+									handleDefaultMessges(tmpMessage);
 									break;
 								}
-							}
-							else if(tmpMessage instanceof ServerStartupMessage) {
+							} 
+							else if (tmpMessage instanceof ServerStartupMessage)
+							{
 								handleStartupMessages((ServerStartupMessage) tmpMessage);
 							}
-						}
-						catch(Exception inEx) {
 							
+						} catch (Exception inEx)
+						{
+
 						}
 					}
 				}
@@ -172,18 +192,22 @@ public class ClientModel {
 			t.setDaemon(true);
 			t.start();
 
-		} catch (Exception inEx) {
+		} catch (Exception inEx)
+		{
 
 		}
 
 	}
-	
+
 	public void disconnect() {
-		if(socket != null) {
-			try {
+		if (socket != null)
+		{
+			try
+			{
 				socket.close();
-			}catch(IOException inEx) {
-				
+			} catch (IOException inEx)
+			{
+
 			}
 		}
 	}
